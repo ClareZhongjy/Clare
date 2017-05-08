1e5d3938423af76a938002ad3eb8665125fc7c6b
ClareZhongjy <609065396@qq.com>
2017-05-08 17:58:18 +0800

    add input stream upload method

--- src/com/SpringMVC/controller/UploadController.java
+++ src/com/SpringMVC/controller/UploadController.java
@@ -1,7 +1,15 @@
 package com.SpringMVC.controller;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 @Controller
 @RequestMapping("/upload")
@@ -10,5 +18,31 @@ public class UploadController {
 	@RequestMapping("/preUpload.do")
 	public String preUpload(){
 		return "upload/upload";
+	}
+	
+	@RequestMapping("/streamUpload.do")
+	public void streamUpload(@RequestParam("file") CommonsMultipartFile file) throws Exception{
+		  try {
+	            //获取输出流
+	            OutputStream os=new FileOutputStream("E:/"+new Date().getTime()+file.getOriginalFilename());
+	            //获取输入流 CommonsMultipartFile 中可以直接得到文件的流
+	            InputStream is=file.getInputStream();
+	            int temp;
+	            //一个一个字节的读取并写入
+	            while((temp=is.read())!=(-1))
+	            {
+	                os.write(temp);
+	            }
+	           os.flush();
+	           os.close();
+	           is.close();
+	         
+	        } catch (FileNotFoundException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
+	    
+	        
+	        
 	}
 }
