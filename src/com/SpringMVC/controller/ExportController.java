2ce6334afbb3be83cbebba95ef49f8c0347a610e
ClareZhongjy <609065396@qq.com>
2017-07-11 17:55:54 +0800

    add export excel

--- src/com/SpringMVC/controller/ExportController.java
+++ src/com/SpringMVC/controller/ExportController.java
@@ -1,12 +1,27 @@
 package com.SpringMVC.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.SpringMVC.entity.Customer;
 import com.SpringMVC.service.ICustomerService;
+import com.SpringMVC.util.ExportExcelUtil;
 
 @Controller
 @RequestMapping("/export")
@@ -15,10 +30,52 @@ public class ExportController {
 	@Autowired
 	ICustomerService cs;
 	
-	@RequestMapping("/exportExcel")
-	public String exportExcel(HttpServletResponse response){
+	
+	@RequestMapping("/preExport.do")
+	public String preExport(){
+		return "export_excel/export_excel";
+	}
+	
+	@RequestMapping("/exportExcel.do")
+	@ResponseBody
+	public String exportExcel(HttpServletRequest request,
+            HttpServletResponse response){
 		
-		
+		response.reset();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddhhmmssms");
+        String dateStr = sdf.format(new Date());
+
+        // 指定下载的文件名
+        response.setHeader("Content-Disposition", "attachment;filename=" +dateStr+".xls");
+        response.setContentType("application/vnd.ms-excel;charset=UTF-8");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setDateHeader("Expires", 0);
+   
+        List<Map<String,String>> list = new ArrayList<Map<String,String>>();
+        List<Customer> rList = cs.getAllCustomer();
+       
+        String[] cols = {"ID","年龄","E-mail","姓名"};
+        for(int i=0;i<rList.size();i++){
+        	Customer c = rList.get(i);
+        	Map<String,String> tempMap = new HashMap<String,String>();
+        	tempMap.put(cols[0], String.valueOf(c.getId()));
+        	tempMap.put(cols[1], String.valueOf(c.getAge()));
+        	tempMap.put(cols[2], String.valueOf(c.getEmail()));
+        	tempMap.put(cols[3], String.valueOf(c.getName()));
+        	list.add(tempMap);
+        }
+        HSSFWorkbook workbook=ExportExcelUtil.generateExcel(list, "test",cols);
+        try {
+            OutputStream output = response.getOutputStream();
+            BufferedOutputStream bufferedOutPut = new BufferedOutputStream(output);
+            bufferedOutPut.flush();
+            workbook.write(bufferedOutPut);
+            bufferedOutPut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 		return null;
 		
 	}
--- src/com/SpringMVC/util/ExportExcelUtil.java
+++ src/com/SpringMVC/util/ExportExcelUtil.java
@@ -1,5 +1,18 @@
 package com.SpringMVC.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.CellRangeAddress;
 import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFFont;
@@ -8,21 +21,23 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExportExcelUtil {
 	
-	private XSSFWorkbook wb = null;
 	
-	private XSSFSheet sheet = null;
 	
-	 
-	public ExportExcelUtil(XSSFWorkbook wb, XSSFSheet sheet)  {  
-	    this.wb = wb;  
-        this.sheet = sheet;  
-	}  
-	 
+	public static XSSFWorkbook generateSExcel(List<Map<String, String>> list, String title) {
+		
+		 XSSFWorkbook wb = new XSSFWorkbook();
+		
+		 XSSFSheet sheet = wb.createSheet("sheet1");
+		
+		return null;  
+		
+	}
+	
 	/**
 	 * 设置表头样式
 	 * @return
 	 */
-	public XSSFCellStyle getHeadStyle(){
+	public XSSFCellStyle getHeadStyle(XSSFWorkbook wb){
 		//创建单元格样式
 		XSSFCellStyle cellStyle = wb.createCellStyle();
 		// 设置单元格颜色
@@ -60,7 +75,7 @@ public class ExportExcelUtil {
 	 * 设置表体的单元格样式
 	 * @return
 	 */
-	public XSSFCellStyle getBodyStyle(){
+	public XSSFCellStyle getBodyStyle(XSSFWorkbook wb){
 		// 创建单元格样式  
 		XSSFCellStyle cellStyle = wb.createCellStyle();  
 		// 设置单元格居中对齐  
@@ -84,4 +99,83 @@ public class ExportExcelUtil {
 		
 		return cellStyle;  
 	}
+	
+	
+	public static HSSFWorkbook generateExcel(List<Map<String, String>> list, String title,String[] cols) {  
+        HSSFWorkbook book = new HSSFWorkbook();  
+        try{  
+          
+          
+                        HSSFSheet sheet = book.createSheet("Sheet1");  
+            sheet.autoSizeColumn(1, true);//自适应列宽度  
+            //样式设置  
+            HSSFCellStyle style = book.createCellStyle();  
+            //style.setFillForegroundColor(HSSFColor.SKY_BLUE.index);  
+             // style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);  
+              style.setBorderBottom(HSSFCellStyle.BORDER_THIN);  
+              style.setBorderLeft(HSSFCellStyle.BORDER_THIN);  
+              style.setBorderRight(HSSFCellStyle.BORDER_THIN);  
+              style.setBorderTop(HSSFCellStyle.BORDER_THIN);  
+              style.setAlignment(HSSFCellStyle.ALIGN_CENTER);  
+              // 生成一个字体  
+              HSSFFont font = book.createFont();  
+              font.setColor(HSSFColor.VIOLET.index);  
+              font.setFontHeightInPoints((short) 12);  
+              font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);  
+              // 把字体应用到当前的样式  
+              style.setFont(font);  
+              
+                
+              HSSFCellStyle style2 = book.createCellStyle();  
+                  //style2.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);  
+                  //设置上下左右边框  
+                  style2.setBorderBottom(HSSFCellStyle.BORDER_THIN);  
+                  style2.setBorderLeft(HSSFCellStyle.BORDER_THIN);  
+                  style2.setBorderRight(HSSFCellStyle.BORDER_THIN);  
+                  style2.setBorderTop(HSSFCellStyle.BORDER_THIN);  
+                  style2.setAlignment(HSSFCellStyle.ALIGN_CENTER);  
+                    
+              //填充表头标题  
+              int colSize = list.get(0).entrySet().size();  
+              System.out.println("size:" + colSize);  
+              //合并单元格供标题使用(表名)  
+             // sheet.addMergedRegion(new CellRangeAddress(0, 0, 0, colSize-1));  
+             // HSSFRow firstRow = sheet.createRow(0);//第几行（从0开始）  
+             // HSSFCell firstCell = firstRow.createCell(0);  
+             // firstCell.setCellValue(title);  
+             // firstCell.setCellStyle(style);  
+                
+              //填充表头header  
+              HSSFRow row = sheet.createRow(1);  
+ 
+             
+              for(int i=0; i< cols.length; i++) {  
+                 
+                  HSSFCell cell = row.createCell(i);  
+                  cell.setCellValue(cols[i]);  
+                  cell.setCellStyle(style2);  
+              }  
+                
+              //填充表格内容  
+              System.out.println("list:" + list.size());  
+              for(int i=0; i<list.size(); i++) {  
+                  HSSFRow row2 = sheet.createRow(i+2);//index：第几行  
+                  Map<String, String> map = list.get(i);  
+                  Set<Entry<String, String>> set2 = map.entrySet();  
+                  List<Entry<String, String>> ll = new ArrayList(set2);  
+                  for(int j=0; j<ll.size(); j++) {  
+                      String val = ll.get(j).getValue();  
+                      HSSFCell cell = row2.createCell(j);//第几列：从0开始  
+                      cell.setCellValue(val);  
+                      cell.setCellStyle(style2);  
+                  }  
+              }  
+                
+//           book.write(fos);   
+//           fos.close();  
+        } catch(Exception ex) {  
+            ex.printStackTrace();  
+        }  
+        return book;  
+    }  
 }
