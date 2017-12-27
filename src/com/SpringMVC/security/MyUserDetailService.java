b5995ce508373b84d5a2e690f1039d93543c83f7
ClareZhongjy <609065396@qq.com>
2017-12-27 14:57:27 +0800

    添加Spring security 3.1.4

---
+++ src/com/SpringMVC/security/MyUserDetailService.java
@@ -0,0 +1,22 @@
+package com.SpringMVC.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailService implements UserDetailsService{
+	
+	//登陆验证时，通过username获取用户的所有权限信息，  
+    //并返回User放到spring的全局缓存SecurityContextHolder中，以供授权器使用
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Collection<GrantedAuthority> auths = new ArrayList<GrantedAuthority>();
+		
+		return null;
+	}
+
+}
